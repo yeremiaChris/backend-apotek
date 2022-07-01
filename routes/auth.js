35e25efdf97ee196f1c8bd15df48fbdbd172409a
yeremiaChris <yeremia997@gmail.com>
2022-07-02 05:42:32 +0700

    commit

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/register", (req, res) => {
-  res.send("register");
+  res.send("registers");
 });
 
 module.exports = router;
